2f0aaa8614a2055ce7e2edeaac70860cae6d4905
waldemarjr <waldemarjr@gmail.com>
2025-08-15 15:14:58 -0300

    Updates

--- scripts/openstack.sh
+++ scripts/openstack.sh
@@ -8,27 +8,32 @@ TERRAFORM_PATH="$USER_HOME/confloss2025/git/confloss2025/terraform"
 
 # Public and private keys - SSH auth - Openstack: controller e compute nodes
 OS_SSH_KEYS_DIR="$USER_HOME/confloss2025/openstackSSHKeys"
-OS_SSH_KEYS_FILE="$OS_SSH_KEYS_DIR/OS_pubprivKeys"
+OS_SSH_KEYS_FILE="$OS_SSH_KEYS_DIR/OS_SSH_KEY"
 
 # Host (openstack nodes) - Ansible inventory
 OS_INVENTORY_DIR="$USER_HOME/confloss2025/git/confloss2025/inventory"
 OS_INVENTORY_FILE="$OS_INVENTORY_DIR/os_nodes"
 
 # Playbooks - deploy configuration - Openstack: controller e compute nodes
-OS_CTRL_PB_DEPLOY="$USER_HOME/confloss2025/ansible/openstack/ctrl_nodes.yml"
-OS_COMP_PB_DEPLOY="$USER_HOME/confloss2025/ansible/openstack/comp_nodes.yml"
+OS_CTRL_PB_DEPLOY="$USER_HOME/confloss2025/git/confloss2025/ansible/openstack/ctrl_nodes.yml"
+OS_COMP_PB_DEPLOY="$USER_HOME/confloss2025/git/confloss2025/ansible/openstack/comp_nodes.yml"
 
 
 clear
 
-echo "+--------------------------------+"
-echo "| Starting infrastructure build  |"
-echo "+--------------------------------+"
+echo "+------------------------------------------+"
+echo "| Starting Openstack infrastructure build  |"
+echo "+------------------------------------------+"
 
 
 echo "Provisioning Cloud Infrastructure..."
-terraform  -chdir=$TERRAFORM_DIR apply -auto-approve
 
+cd $TERRAFORM_PATH
+terraform apply -auto-approve
+
+sleep 5
+
+cd - 1>/dev/null 2>/dev/null
 
 echo "+------------------+"
 echo "| Controller nodes |"
@@ -37,13 +42,13 @@ echo "+------------------+"
 rm -f $OS_SSH_KEYS_DIR/*
 ssh-keygen -t rsa -b 4096 -N "" -f $OS_SSH_KEYS_FILE 1> /dev/null
 
-ansible-playbook -i $OS_INVENTORY_FILE $OS_CTRL_PB_DEPLOY
+ansible-playbook -i "$OS_INVENTORY_FILE" "$OS_CTRL_PB_DEPLOY"
 
-sleep 3
-clear
+sleep 5
+
 
 echo "+-------------------+"
 echo "|   Compute nodes   |"
 echo "+-------------------+"
 
-ansible-playbook -i $OS_INVENTORY_FILE $OS_COMP_PB_DEPLOY 
+ansible-playbook -i "$OS_INVENTORY_FILE" "$OS_COMP_PB_DEPLOY" 
