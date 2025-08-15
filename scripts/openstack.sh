fa220897ffd10256947f87b936c66915b6090686
waldemarjr <waldemarjr@gmail.com>
2025-08-15 14:41:50 -0300

    add: new files

--- scripts/openstack.sh
+++ scripts/openstack.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+
+# User Home
 USER_HOME="/home/waldemar"
+
+# Terraform files - IaC provisioning infrastructure.
+
+
+# Public and private keys - SSH auth - Openstack: controller e compute nodes
+OS_SSH_KEYS_DIR="$USER_HOME/confloss2025/openstackSSHKeys"
+OS_SSH_KEYS_FILE="$OS_SSH_KEYS_DIR/OS_pubprivKeys"
+
+# Host (openstack nodes) - Ansible inventory
+OS_INVENTORY_DIR="$USER_HOME/confloss2025/git/confloss2025/inventory"
+OS_INVENTORY_FILE="$OS_INVENTORY_DIR/os_nodes"
+
+# Playbooks - deploy configuration - Openstack: controller e compute nodes
+OS_CTRL_PB_DEPLOY="$USER_HOME/confloss2025/ansible/openstack/ctrl_nodes.yml"
+OS_COMP_PB_DEPLOY="$USER_HOME/confloss2025/ansible/openstack/comp_nodes.yml"
+
 
 clear
 
@@ -8,14 +26,19 @@ echo "| Starting infrastructure build  |"
 echo "+--------------------------------+"
 
 
+echo "Provisioning Cloud Infrastructure..."
+
+terraform apply -auto-approve
+
+
 echo "+------------------+"
 echo "| Controller nodes |"
 echo "+------------------+"
 
-rm -f $USER_HOME/confloss2025/openstackSSHKeys/*
-ssh-keygen -t rsa -b 4096 -N "" -f $USER_HOME/confloss2025/openstackSSHKeys/ctrl_node 1> /dev/null
+rm -f $OS_SSH_KEYS_DIR/*
+ssh-keygen -t rsa -b 4096 -N "" -f $OS_SSH_KEYS_FILE 1> /dev/null
 
-ansible-playbook -i ../inventory/os_nodes ../ansible/openstack/ctrl_nodes.yml
+ansible-playbook -i $OS_INVENTORY_FILE $OS_CTRL_PB_DEPLOY
 
 sleep 3
 clear
@@ -24,5 +47,4 @@ echo "+-------------------+"
 echo "|   Compute nodes   |"
 echo "+-------------------+"
 
-ansible-playbook -i ../inventory/os_nodes ../ansible/openstack/comp_nodes.yml 
-
+ansible-playbook -i $OS_INVENTORY_FILE $OS_COMP_PB_DEPLOY 
